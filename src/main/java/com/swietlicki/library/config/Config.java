d46db3c21cb3361d445b4d0d99a4af35c3daf81c
Krzysztof Swietlicki <k.k.swietlicki@gmail.com>
2022-05-17 20:42:10 +0200

    Spring Security implementation - JSON authentication

--- src/main/java/com/swietlicki/library/config/Config.java
+++ src/main/java/com/swietlicki/library/config/Config.java
@@ -1,6 +1,5 @@
 package com.swietlicki.library.config;
 
-import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
---
+++ src/main/java/com/swietlicki/library/config/LoginCredentials.java
@@ -0,0 +1,10 @@
+package com.swietlicki.library.config;
+
+import lombok.Getter;
+
+@Getter
+public class LoginCredentials {
+
+    private String username;
+    private String password;
+}
